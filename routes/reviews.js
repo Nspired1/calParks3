292f2b2dec7a5a13ed552e9a423c43814ef599c1
Nspired1 <don.spire1@gmail.com>
2021-07-25 21:08:27 -0700

    add mergeParams to router

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const Park = require("../models/park");
 const Review = require("../models/review");
 const catchAsync = require("../utils/catchAsync");
