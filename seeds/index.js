87cd82fe1e54cbec0365522d5078f9d5c22e41ae
Nspired1 <don.spire1@gmail.com>
2021-07-25 17:00:15 -0700

    remove old reviews on seeding

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Park = require("../models/park");
+const Review = require("../models/review");
 
 // mongoose settings and connection
 mongoose.connect("mongodb://localhost:27017/calparks3", {
@@ -24,9 +25,10 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   //delete everything in database
   await Park.deleteMany({});
+  await Review.deleteMany({});
   //loop over
-  for (let i = 0; i < 40; i++) {
-    const random40 = Math.floor(Math.random() * 40);
+  for (let i = 0; i < 5; i++) {
+    const random40 = Math.floor(Math.random() * 5);
     const park = new Park({
       location: `${cities[random40].city}, ${cities[random40].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
@@ -41,5 +43,5 @@ const seedDB = async () => {
 // to run seed file at command prompt at root dir write: node seeds/index.js
 seedDB().then(() => {
   mongoose.connection.close();
-  console.log("Close connection to local database");
+  console.log("Closed connection to local database");
 });
