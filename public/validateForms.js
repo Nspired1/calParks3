be3591592c229efd0bc8264e6359f93bb44e798b
Nspired1 <don.spire1@gmail.com>
2021-07-26 20:33:50 -0700

    add public dir

---
+++ public/validateForms.js
@@ -0,0 +1,21 @@
+// JS to enable Bootstrap form validation
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    // old way to make an array, could use Array.from(forms)
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+      })()
