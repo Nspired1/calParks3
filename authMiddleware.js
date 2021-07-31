6288c686d8e30244c35535ffd75fd501ef447d2d
Nspired1 <don.spire1@gmail.com>
2021-07-30 22:27:42 -0700

    update validations

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,3 +1,45 @@
+const Joi = require('joi');
+const { reviewSchema, parkSchema } = require('./joiSchemas.js');
+//const { reviewSchema } = require('./joiSchemas.js');
+const ExpressError = require("./utils/ExpressError");
+const Park = require("./models/park");
+const Review = require("./models/review");
+
+
+// Joi validation middleware for parks
+module.exports.validatePark = (req, res, next) => {
+    // const parkSchema = Joi.object({
+    //     park: Joi.object({
+    //         title: Joi.string().required(),
+    //         description: Joi.string().required(),
+    //         location: Joi.string().required(),
+    //         price: Joi.number().min(0).allow(null, ''),
+    //         image: Joi.string().allow(null, '')
+    //     }).required()
+    // });
+    const { error } = parkSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(element => element.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+//  joi validation middleware
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(element => element.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+
+// this is auth and validation middleware
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
@@ -6,3 +48,26 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
+
+// check for author of park middleware
+module.exports.isAuthor = async(req, res, next) => {
+    const { id } = req.params;
+    const park = await Park.findById(id);
+    if(!park.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that.');
+        return res.redirect(`/parks/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that.');
+        return res.redirect(`/parks/${id}`);
+    }
+    next();
+}
+
+
--- joiSchemas.js
+++ joiSchemas.js
@@ -1,6 +1,19 @@
 // Joi schemas are VALIDATION schemas, not Database schemas
 const Joi = require('joi');
 
+// Joi schema for required fields for parks
+module.exports.parkSchema = Joi.object({
+    park: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        price: Joi.number().min(0).allow(null, ''),
+        image: Joi.string().allow(null, '')
+    }).required(),
+    deleteImages: Joi.array()
+});
+
+// Joi schema for required fields for reviews
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
