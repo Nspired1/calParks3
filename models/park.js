ff7699fad9b8ba8aaff45c9f9721869b5ec29ab0
Nspired1 <don.spire1@gmail.com>
2021-07-30 22:43:33 -0700

    add thumbnails to edit view & park model

--- models/park.js
+++ models/park.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function (){
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const ParkSchema = new Schema({
     title: String,
     price: Number,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ ImageSchema ],
     description: String,
     location: String,
     author: {
