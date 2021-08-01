30091fffa7a6aa172ec1c08703a45429c1b9a3fe
Nspired1 <don.spire1@gmail.com>
2021-08-01 09:47:02 -0700

    add cluster map

--- models/park.js
+++ models/park.js
@@ -1,54 +1,67 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    url: String,
-    filename: String
+  url: String,
+  filename: String,
 });
 
-ImageSchema.virtual('thumbnail').get(function (){
-    return this.url.replace('/upload', '/upload/w_200');
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
 });
 
-const ParkSchema = new Schema({
+const ParkSchema = new Schema(
+  {
     title: String,
     price: Number,
-    images: [ ImageSchema ],
+    images: [ImageSchema],
     description: String,
     location: String,
     geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     author: {
-        type: Schema.Types.ObjectId,
-        ref:'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true }, //enables virtual properties to populate when converting to JSON
+  }
+);
+
+// virtual schema for Mapbox. Populates park pin pop up info
+// also by default Mongoose doesn't include virtuals when you convert to json.
+// see above for toJSON
+ParkSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<a href="/parks/${this._id}">${this.title}</a>
+            <p>${this.description.substring(0, 20)}...</p>`;
 });
 
 // cascade delete for reviews when a Park is deleted
-ParkSchema.post('findOneAndDelete', async function(doc){
-    if(doc){
-        await Review.deleteMany({
-            _id : {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+ParkSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Park', ParkSchema);
+module.exports = mongoose.model("Park", ParkSchema);
